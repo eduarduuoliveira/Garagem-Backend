adf833de62787f600eb1582a0954388ff53a9c67
eduaruuoliveira <ayla.livi@gmail.com>
2024-04-03 12:25:17 -0300

    criação dos serializers

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,1 +1,5 @@
 from .user import UserSerializer
+from .acessorio import AcessorioSerializer
+from .categoria import CategoriaSerializer
+from .marca import MarcaSerializer
+from .cor import CorSerializer
